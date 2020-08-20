a728da23927181a5eec088478c3d80d13991ba7d
Maria Zafar <m.zafar@khi.iba.edu.pk>
2020-08-21 00:12:34 +0500

    Deleted a file and modified

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> This App is being created by Maria. and SHe will be mking some really wonder app using React. No Doubt!</p>
       </div>
     );
   }
